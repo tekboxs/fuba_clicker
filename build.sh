5696614f97b4752137c40321e3d28f766fbc7058
tekboxs <tekboxs@gmail.com>
2025-10-15 18:03:28 -0400

    ver3

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+echo "Setting up Flutter environment..."
+
+# Clone or update Flutter
+if cd flutter; then
+  git pull && cd ..
+else
+  git clone https://github.com/flutter/flutter.git
+fi
+
+# Add Flutter to PATH
+export PATH="$PATH:$(pwd)/flutter/bin"
+
+# Configure Flutter
+flutter doctor
+flutter config --enable-web
+flutter pub get
+
+# Build the project
+flutter build web --release --web-renderer html --base-href /
+
+echo "Build completed successfully!"
